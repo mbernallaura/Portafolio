c08b91d5d7a5a973b5242e6f6f31badd2ab4f85c
laura <mbernallaura@gmail.com>
2024-10-15 21:16:57 -0500

    Agregar mas habilidades

--- src/app/skills/page.jsx
+++ src/app/skills/page.jsx
@@ -6,15 +6,18 @@ const SkillsPage = () => {
     const skills = [
         { id: 1, name: 'Next.js', image: './images/Skills/nextjs.svg' },
         { id: 2, name: 'React', image:'./images/Skills/react.svg' },
-        { id: 3, name: 'JavaScript', image: './images/Skills/javascript.svg' },
-        { id: 4, name: 'Html', image: './images/Skills/html.svg' },
-        { id: 5, name: 'Css', image: './images/Skills/css.svg' },
-        { id: 6, name: 'Sass', image: './images/Skills/sass.svg' },
-        { id: 7, name: 'Tailwind', image: './images/Skills/tailwind.svg' },
-        { id: 8, name: 'MySql', image: './images/Skills/mysql.svg' },
-        { id: 9, name: 'Firebase', image: './images/Skills/firebase.svg' },
-        { id: 10, name: 'Git', image: './images/Skills/git.svg' },
-        { id: 11, name: 'Github', image: './images/Skills/github.svg' },
+        { id: 3, name: 'Redux', image: './images/Skills/redux.svg' },
+        { id: 4, name: 'JavaScript', image: './images/Skills/javascript.svg' },
+        { id: 5, name: 'Html', image: './images/Skills/html.svg' },
+        { id: 6, name: 'Css', image: './images/Skills/css.svg' },
+        { id: 7, name: 'Sass', image: './images/Skills/sass.svg' },
+        { id: 8, name: 'Tailwind', image: './images/Skills/tailwind.svg' },
+        { id: 9, name: 'Material UI', image: './images/Skills/materialUI.svg' },
+        { id: 10, name: 'MySql', image: './images/Skills/mysql.svg' },
+        { id: 11, name: 'Firebase', image: './images/Skills/firebase.svg' },
+        { id: 12, name: 'Airtable', image: './images/Skills/airtable.svg' },
+        { id: 13, name: 'Git', image: './images/Skills/git.svg' },
+        { id: 14, name: 'Github', image: './images/Skills/github.svg' },
     ]
 
     return (
